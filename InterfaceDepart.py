c690c48031b8a6d9acb64f1049807a73fd5e8a52
Thomas <thomas.routin.du21@gmail.com>
2022-05-05 16:46:12 +0200

    Cration du bouton quitter

--- InterfaceDepart.py
+++ InterfaceDepart.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 pygame.init()
 
 #Creation de la fenetre
-fenetre = pygame.display.set_mode((720, 720))
+fenetre = pygame.display.set_mode((400, 560))
 
 #Ajout d'un fond
 fond = pygame.image.load("TEST.jpg").convert()
@@ -38,6 +38,7 @@ height = fenetre.get_height()
 smallfont = pygame.font.SysFont('Corbel',35)
 
 textquit = smallfont.render('QUIT' , True , color)
+textplay = smallfont.render('PLAY' , True , color)
 
 
 
@@ -50,18 +51,28 @@ while continuer:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			continuer = 0
-		if event.type == pygame.MOUSEBUTTONDOWN:
+		if event.type == pygame.MOUSEBUTTONDOWN: #si un clique sur le bouton quitter
 			if width/2 <= mouse[0] <= width/2 +140 and height/2 <= mouse[1] <= height/2+40:
 				continuer = 0
+		if event.type == pygame.MOUSEBUTTONDOWN: #si un clique sur le bouton play
+			if width/2 <= mouse[0] <= width/2 +140 and height/2.4 <= mouse[1] <= height/2.4+40:
+				continuer = 0
 
+	mouse = pygame.mouse.get_pos() #obtenir la position du curseur
 
-	mouse = pygame.mouse.get_pos()
-
+#couleur du bouton plus clair quand la souris passe dessus (quit)
 	if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
 		pygame.draw.rect(fenetre,color_light,[width/2,height/2,140,40])
-
+#sinon plus sombre
 	else:
 		pygame.draw.rect(fenetre,color_dark,[width/2,height/2,140,40])
 	fenetre.blit(textquit , (width/2+50,height/2))
+#idem pour Play
+	if width/2 <= mouse[0] <= width/2+140 and height/2.4 <= mouse[1] <= height/2.4+40:
+		pygame.draw.rect(fenetre,color_light,[width/2,height/2.4,140,40])
+
+	else:
+		pygame.draw.rect(fenetre,color_dark,[width/2,height/2.4,140,40])
+	fenetre.blit(textplay , (width/2+50,height/2.4))
 
 	pygame.display.update()
