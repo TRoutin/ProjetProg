25ac933f9c3fb1e58b42f041be7a2f0b2dfa0df1
Moustafa Kobeissi <moustafa.kobeissi@hotmail.fr>
2022-05-17 16:13:03 +0200

    Modification des variables afin de pouvoir changer la taille du BOARD

--- main.py
+++ main.py
@@ -15,45 +15,49 @@ Value = InterfaceDepart()
 
 
 if Value == 1:
+    TILESWIDTH = 10
+    TILESHEIGHT = 14
 
-
-    WIDTH = 400
-    HEIGHT = 560
+    WIDTH = TILESWIDTH * 40
+    HEIGHT = TILESHEIGHT * 40
     TITLE = "Alder Crush"
 
     cursor = Actor("selected", topleft=(0, 0))
 
 
-    def cursor_tile_pos():
-        return (int(cursor.x // 40) - 1, int(cursor.y // 40))
 
+
+
+    board = []
+    for row in range(TILESHEIGHT):
+        tiles = [random.randint(1, 8) for x in range(TILESWIDTH)]
+        board.append(tiles)
 
     def draw():
-        for y in range(14):
-            for x in range(10):
+        for y in range(TILESHEIGHT):
+            for x in range(TILESWIDTH):
                 tile = board[y][x]
                 screen.blit(str(tile), (x * 40, y * 40))
         cursor.draw()
 
+    def cursor_tile_pos():
+        return int(cursor.x // 40) - 1, int(cursor.y // 40)
 
     def on_key_up(key):
-        x, y =cursor_tile_pos()
+        x, y = cursor_tile_pos()
         if key == keys.LEFT and x > 0:
             cursor.x -= 40
-        if key == keys.RIGHT and x < 8:
+        if key == keys.RIGHT and x < TILESWIDTH - 2:
             cursor.x += 40
         if key == keys.UP and y > 0 :
             cursor.y -= 40
-        if key == keys.DOWN and y < 13:
+        if key == keys.DOWN and y < TILESHEIGHT - 1:
             cursor.y += 40
         if key == keys.SPACE:
             x, y = cursor_tile_pos()
             board[y][x], board[y][x + 1] = board[y][x + 1], board[y][x]
 
 
-    board = []
-    for row in range(14):
-        tiles = [random.randint(1, 8) for x in range(10)]
-        board.append(tiles)
+
 
     pgzrun.go()
