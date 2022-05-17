ea94bdd2790bc936db01bbbbb193a73d169892e0
Thomas <thomas.routin.du21@gmail.com>
2022-05-17 17:25:21 +0200

    Creation d'un bouton pour couper le son et le remettre

--- InterfaceDepart.py
+++ InterfaceDepart.py
@@ -8,11 +8,13 @@ def InterfaceDepart() :
 	pygame.init()
 
 	#Creation de la fenetre
-	fenetre = pygame.display.set_mode((400, 560))
+	screen = pygame.display.set_mode((400, 560))
 
 	#Ajout d'un fond
-	fond = pygame.image.load("TEST.jpg").convert()
-	fenetre.blit(fond, (0,0))
+	background = pygame.image.load("TEST.jpg").convert()
+	sound = pygame.image.load('sound.png').convert_alpha()
+	screen.blit(background, (0,0))
+	screen.blit(sound, (250,10))
 	pygame.display.set_caption('Pregame')
 
 	#refresh de le fenetre
@@ -33,14 +35,17 @@ def InterfaceDepart() :
 
 	color_dark = (100,100,100)
 
-	width = fenetre.get_width()
+	width = screen.get_width()
 
-	height = fenetre.get_height()
+	height = screen.get_height()
 
 	smallfont = pygame.font.SysFont('Corbel',35)
 
 	textquit = smallfont.render('QUIT' , True , color)
 	textplay = smallfont.render('PLAY' , True , color)
+	textOFF = smallfont.render('Off' , True , color)
+	textON = smallfont.render('On' , True , color)
+
 
 
 
@@ -61,23 +66,44 @@ def InterfaceDepart() :
 				if width/2-70 <= mouse[0] <= width/2+70 and height/2.4 <= mouse[1] <= height/2.4+40:
 					continuer = 0
 					return 1
+				if 350 <= mouse[0] <= 390 and 10 <= mouse[1] <= 50:
+					pygame.mixer.music.pause()
+				if 300 <= mouse[0] <= 340 and 10 <= mouse[1] <= 50:
+					pygame.mixer.music.unpause()
 
 		mouse = pygame.mouse.get_pos() #obtenir la position du curseur
 
 	#couleur du bouton plus clair quand la souris passe dessus (quit)
 		if width/2-70 <= mouse[0] <= width/2+70 and height/1.1 <= mouse[1] <= height/1.1+40:
-			pygame.draw.rect(fenetre,color_light,[width/2-70,height/1.1,140,40])
+			pygame.draw.rect(screen,color_light,[width/2-70,height/1.1,140,40])
 	#sinon plus sombre
 		else:
-			pygame.draw.rect(fenetre,color_dark,[width/2-70,height/1.1,140,40])
-		fenetre.blit(textquit , (width/2-30,height/1.1+7))
+			pygame.draw.rect(screen,color_dark,[width/2-70,height/1.1,140,40])
+		screen.blit(textquit , (width/2-30,height/1.1+7))
 	#idem pour Play
 		if width/2-70 <= mouse[0] <= width/2+70 and height/2.4 <= mouse[1] <= height/2.4+40:
-			pygame.draw.rect(fenetre,color_light,[width/2-70,height/2.4,140,40])
+			pygame.draw.rect(screen,color_light,[width/2-70,height/2.4,140,40])
 
 		else:
-			pygame.draw.rect(fenetre,color_dark,[width/2-70,height/2.4,140,40])
-		fenetre.blit(textplay , (width/2-30,height/2.4+7))
+			pygame.draw.rect(screen,color_dark,[width/2-70,height/2.4,140,40])
+		screen.blit(textplay , (width/2-30,height/2.4+7))
+
+
+
+		if 350 <= mouse[0] <= 390 and 10 <= mouse[1] <= 50:
+			pygame.draw.rect(screen,color_light,[350,10,40,40])
+
+		else:
+			pygame.draw.rect(screen,color_dark,[350,10,40,40])
+		screen.blit(textOFF , (352,10+7))
+
+		if 300 <= mouse[0] <= 340 and 10 <= mouse[1] <= 50:
+			pygame.draw.rect(screen,color_light,[300,10,40,40])
+
+		else:
+			pygame.draw.rect(screen,color_dark,[300,10,40,40])
+		screen.blit(textON , (302,10+7))
 
 		pygame.display.update()
+
 
